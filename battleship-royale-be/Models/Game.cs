01260c435cff4631b7a7fd419aedc3d8b2719dd9
Lukas Kuzmickas <lukas6bkuzmickas@gmail.com>
2024-11-03 15:06:50 +0200

    singleton

--- battleship-royale-be/Models/Game.cs
+++ battleship-royale-be/Models/Game.cs
@@ -1,10 +1,22 @@
-﻿namespace battleship_royale_be.Models
+﻿using System;
+using System.Collections.Generic;
+
+namespace battleship_royale_be.Models
 {
     public class Game
     {
+        private static Game _instance;
+
         public Guid Id { get; set; }
         public List<Player> Players { get; set; }
         public string ShotResultMessage { get; set; }
+
+        private Game(Guid Id, List<Player> Players, string ShotResultMessage)
+        {
+            this.Id = Id;
+            this.Players = Players;
+            this.ShotResultMessage = ShotResultMessage;
+        }
 
         public Game() { }
 
@@ -13,6 +25,18 @@
             this.Id = Id;
             this.Players = Players;
             this.ShotResultMessage = ShotResultMessage;
+        }
+
+        public static Game Instance(Guid Id, List<Player> Players, string ShotResultMessage)
+        {
+            get
+            {
+                if (_instance == null)
+                {
+                    _instance = new Game(Id, Players, ShotResultMessage);
+                }
+                return _instance; 
+            }
         }
     }
 }
